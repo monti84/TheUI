2747af249d494c0adda6814584a564336694a058
Attila Nagy <attila.nagy.monti@gmail.com>
2021-02-01 20:12:41 +0100

    Improve modal.

--- assets/js/theui.modal.js
+++ assets/js/theui.modal.js
@@ -1,8 +1,19 @@
 TheUI.Modal = {
 	GlobalSettings: {
-		CloseOnBackDropClick: true
+		CloseOnBackDropClick: true,
+		CloseOnEscape: false
 	},
 	Init () {
+		document.addEventListener("keydown", event => {
+			if (event.code === 'Escape') {
+				let openedModalID = this.GetOpenedModalID();
+				if (openedModalID === '') return true; 
+				let modalSettings = this.GetSettings(openedModalID);
+				if (modalSettings.CloseOnEscape === true) this.CloseModal();
+				event.preventDefault();
+			  	event.stopPropagation;
+			}
+		  });
 		let modalContainers = document.querySelectorAll('.modal-container');
 		let modalSummonerButtons = document.querySelectorAll('*[data-modal_target_id]');		
 		modalSummonerButtons.forEach((modalSummonerButton) => {
@@ -18,13 +29,26 @@ TheUI.Modal = {
 		let modalContainer = document.querySelector('*[data-modal_id="' + id + '"]');
 		if (modalContainer === null) { console.info('Modal window not found with the specified id: "' + id + '".'); return false; }
 		let modalClone = modalContainer.cloneNode(true);
-		let closeButton = modalClone.querySelector('.modal-close');
+		let closeButtons = modalClone.querySelectorAll('.modal-close');
 		let body = document.querySelector('body');
 		let modalSettings = this.GetSettings(id);
 		
-		closeButton.addEventListener('click', (event) => { let backDrop = document.querySelector('body > .modal-backdrop'); if (backDrop) backDrop.remove(); });
-		if (modalSettings.CloseOnBackDropClick === true) backDrop.addEventListener('click', (event) => { let backDrop = document.querySelector('body > .modal-backdrop'); if (backDrop) backDrop.remove(); });
+		if (modalSettings.CloseOnBackDropClick === true) backDrop.addEventListener('click', (event) => { this.CloseModal(); });
 
+		closeButtons.forEach((button) => {
+			button.addEventListener('click', (event) => { this.CloseModal(id) });
+		});
+
+		let modalSummonerButtons = modalClone.querySelectorAll('*[data-modal_target_id]');		
+		modalSummonerButtons.forEach((modalSummonerButton) => {
+			modalSummonerButton.addEventListener('click', (event) => {
+				let modalId = modalSummonerButton.getAttribute('data-modal_target_id');
+				this.OpenModal(modalId);
+			});
+		});
+
+
+		this.CloseModal();
 		body.classList.add('modal-opened');
 		modalClone.classList.add('modal-attached');
 		backDrop.classList.add('modal-backdrop');
@@ -51,8 +75,19 @@ TheUI.Modal = {
 		return retSettings;
 	},
 
-	CloseModal(id) {
+	CloseModal() {
 		document.querySelector('body').classList.remove('modal-opened');
+		let backDrop = document.querySelector('body > .modal-backdrop');
+		if (backDrop) backDrop.remove(); 
+	},
+
+	GetOpenedModalID () {
+		let ret = '';
+		let backDrop = document.querySelector('body > .modal-backdrop');
+		if (backDrop === null) return ret;
+		let modalContainer = backDrop.querySelector('.modal-container');
+		let modalId = modalContainer.getAttribute('data-modal_id')
+		return modalId;
 	}
 }
 
