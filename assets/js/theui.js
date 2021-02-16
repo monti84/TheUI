a7e099f6fa350fb40f9906554e44c58404c4bee2
Attila Nagy <attila.nagy.monti@gmail.com>
2021-02-16 18:01:42 +0100

    Update .gtignore. Improve modal, handle multiple instances. Dummy images linked locally.

--- assets/js/theui.js
+++ assets/js/theui.js
@@ -1,1 +1,19 @@
 var TheUI = TheUI || {};
+
+TheUI.Utils = {
+    SearchInArray (list, key, value, returnOnFirstMatch) {
+        console.log('SearchInArray', list, key, value, returnOnFirstMatch);
+        let results;
+        if (returnOnFirstMatch) results = false;
+        if (!list || list.length < 1) return results;
+
+        for (var x = 0; x < list.length; x++) {
+          console.log(list[x][key] == value);
+            if (list[x][key] == value) {
+            if (returnOnFirstMatch === true) return list[x];
+            else results.push(list[x]);
+          }
+        }
+        return results;
+    }
+}
--- assets/js/theui.modal.js
+++ assets/js/theui.modal.js
@@ -3,6 +3,9 @@ TheUI.Modal = {
 		CloseOnBackDropClick: true,
 		CloseOnEscape: false
 	},
+
+	Modals: [],
+
 	Init () {
 		document.addEventListener("keydown", event => {
 			if (event.code === 'Escape') {
@@ -16,19 +19,48 @@ TheUI.Modal = {
 		});
 
 		let modalSummonerButtons = document.querySelectorAll('*[data-modal_target_id]');		
+	
 		modalSummonerButtons.forEach((modalSummonerButton) => {
+			let modalId = modalSummonerButton.getAttribute('data-modal_target_id');
+			let ModalInstance = TheUI.Utils.SearchInArray(this.Modals, 'Id', modalId, true);
+			console.log(modalId, ModalInstance);
+			if (ModalInstance === false) {
+				ModalInstance = {
+					Id: modalId,
+					Settings: this.GetSettings(modalId),
+					SummonerButtons: new Array(modalSummonerButton),
+					ModalElement: document.querySelector('*[data-modal_id="' + modalId + '"]')
+				}
+				
+				this.Modals.push(ModalInstance);
+				console.log(this.Modals);
+			}
+
+			else {
+				let ButtonIndex = ModalInstance.SummonerButtons.indexOf(modalSummonerButton);
+				if (ButtonIndex < 0) ModalInstance.SummonerButtons.push(modalSummonerButton);
+			}
+
 			modalSummonerButton.addEventListener('click', (event) => {
-				let modalId = modalSummonerButton.getAttribute('data-modal_target_id')
 				this.OpenModal(modalId);
 				event.preventDefault();
 			  	event.stopPropagation;
 			});
 		});
-	},	
+
+		console.log(this.Modals);
+	},
+
+	GetModal(modalId) {
+		let ModalInstance = TheUI.Utils.SearchInArray(this.Modals, 'Id', modalId, true);
+		return ModalInstance;
+	},
 
 	OpenModal(modalId) {
 		let backDrop = document.createElement('div');
-		let modalContainer = document.querySelector('*[data-modal_id="' + modalId + '"]');
+		let ModalInstance = this.GetModal(modalId);
+		let modalContainer = ModalInstance.ModalElement;
+
 		if (modalContainer === null) { console.info('Modal window not found with the specified id: "' + modalId + '".'); return false; }
 		let modalClone = modalContainer.cloneNode(true);
 		let closeButtons = modalClone.querySelectorAll('.modal-close');
@@ -51,6 +83,11 @@ TheUI.Modal = {
 
 
 		this.CloseModal();
+		let ModalButtons = ModalInstance.SummonerButtons;
+		console.log(ModalButtons);
+		ModalButtons.forEach((ModalButton) => {
+			ModalButton.classList.add('modal-opened');
+		});
 		body.classList.add('modal-opened');
 		modalClone.classList.add('modal-attached');
 		backDrop.classList.add('modal-backdrop');
@@ -78,6 +115,13 @@ TheUI.Modal = {
 	},
 
 	CloseModal() {
+		this.Modals.forEach((Modal) => {
+			console.log(Modal);
+			Modal.SummonerButtons.forEach((ModalButton) => {
+				ModalButton.classList.remove('modal-opened');
+			});
+		});
+
 		document.querySelector('body').classList.remove('modal-opened');
 		let backDrop = document.querySelector('body > .modal-backdrop');
 		if (backDrop) backDrop.remove(); 
